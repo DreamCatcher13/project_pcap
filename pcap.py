2814855f13a8b9692f7d377726b69ebd76b5514c
DreamCatcher13 <gerey37ng@gmail.com>
2022-01-10 13:55:13 +0200

    Add files via upload
    made a class Analyzer and put all functions here as static methods

--- pcap.py
+++ pcap.py
@@ -4,192 +4,200 @@ import datetime
 import os
 import argparse
 import socket
+
+class Analyzer():
+    """packet analyzer itself"""	
+
+    @staticmethod
+    def find_session(pkts, IP_addr):
+        """to get pkts related to specific IP"""
+        session = {}
+        for pkt in pkts:
+            if pkt.haslayer(IP):
+                if pkt[IP].src == IP_addr:
+                    seg = pkt[IP].payload.name
+                    time = str(datetime.datetime.fromtimestamp(float(pkt.time)))
+                    session[f"{seg}: {pkt[IP].src} --> {pkt[IP].dst}"] = time
+                elif pkt[IP].dst == IP_addr:
+                    seg = pkt[IP].payload.name
+                    time = str(datetime.datetime.fromtimestamp(float(pkt.time)))
+                    session[f"{seg}: {pkt[IP].dst} <-- {pkt[IP].src}"] = time
+                else:
+                    continue
+            else:
+                continue
+        result_d = {value : key for (key, value) in session.items()}
+        sort = sorted(result_d)
+        result = []
+        for i in sort:
+            result.append(f"{i}  {result_d[i]}")
+        result = "\n".join(result)
+        result = "\n\n".join((f"Unique connections from {IP_addr} OR to {IP_addr} sorted by time", result))
+        return result
+
+    @staticmethod
+    def session(pkts, IP_addr_1, IP_addr_2):
+        """to get pkts related to specific session between 2 ip"""
+        session = []
+        packets = PacketList()
+        for pkt in pkts:
+            if pkt.haslayer(IP) and not pkt.haslayer(ICMP):
+                if pkt[IP].src == IP_addr_1 and pkt[IP].dst == IP_addr_2:
+                    time = datetime.datetime.fromtimestamp(float(pkt.time))
+                    seg = pkt[IP].payload.name
+                    sport = pkt[seg].sport
+                    dport = pkt[seg].dport
+                    p_number = pkts.index(pkt)+1
+                    session.append(f"#{p_number} {time}  {seg}:  {pkt[IP].src}:{sport} --> {pkt[IP].dst}:{dport}")
+                    packets.append(pkt)
+                elif pkt[IP].src == IP_addr_2 and pkt[IP].dst == IP_addr_1:
+                    time = datetime.datetime.fromtimestamp(float(pkt.time))
+                    seg = pkt[IP].payload.name
+                    sport = pkt[seg].sport
+                    dport = pkt[seg].dport
+                    p_number = pkts.index(pkt)+1
+                    session.append(f"#{p_number} {time}  {seg}:  {pkt[IP].dst}:{dport} <-- {pkt[IP].src}:{sport}")
+                    packets.append(pkt)
+                else:
+                    continue
+            elif pkt.haslayer(IP) and pkt.haslayer(ICMP):
+                if pkt[IP].src == IP_addr_1 and pkt[IP].dst == IP_addr_2:
+                    time = datetime.datetime.fromtimestamp(float(pkt.time))
+                    seg = pkt[IP].payload.name
+                    p_number = pkts.index(pkt)+1
+                    session.append(f"#{p_number} {time}  {seg}:  {pkt[IP].src} --> {pkt[IP].dst}")
+                    packets.append(pkt)
+                elif pkt[IP].src == IP_addr_2 and pkt[IP].dst == IP_addr_1:
+                    time = datetime.datetime.fromtimestamp(float(pkt.time))
+                    seg = pkt[IP].payload.name
+                    p_number = pkts.index(pkt)+1
+                    session.append(f"#{p_number} {time}  {seg}:  {pkt[IP].dst} <-- {pkt[IP].src}")
+                    packets.append(pkt)
+                else:
+                    continue
+            else:
+                continue
+            session = "\n".join(session)
+            result = "\n\n".join((f"Packets exchange between {IP_addr_1} and {IP_addr_2}", session))
+            return result, packets
+
+    @staticmethod
+    def list_of_IP(pkts):
+        """private and public IP"""
+        unique_ip = []
+        private_ip = []
+        public_ip = {}
+        dns_A = PacketList()
+        dns_ans = {}
+        prv_ip = re.compile(r"(10\.\d{1,3}\.\d{1,3}\.\d{1,3})|(172\.((1[6-9])|2([0-9])|(3[0-2]))\.\d{1,3}\.\d{1,3})|(192\.168\.\d{1,3}\.\d{1,3})")
+        for p in pkts[IP]:
+            if p.haslayer(IP):
+                f = p[IP]
+                src = f[IP].src
+                dst = f[IP].dst
+                unique_ip.append(src)
+                unique_ip.append(dst)
+                if f.payload.name == "UDP" and f[UDP].sport == 53 and f[DNS].an != None and f[DNS].an.type == 1:
+                    dns_A.append(p)
+        for i in set(unique_ip):
+            mo = prv_ip.search(i)
+            if mo == None:
+                public_ip[i] = ''
+            else:
+                private_ip.append(mo.group())
+        for p in dns_A:
+            dns_ans[p[DNS].an.rdata] = p[DNS].an.rrname.decode("utf-8")
+        for i in dns_ans.keys():
+            if i in public_ip:
+                public_ip[i] = dns_ans[i]
+        private_ip = "\n".join(private_ip)
+        result = "\n\n".join(("Private IP addresses", private_ip))
+        public = []
+        for i in public_ip:
+            if public_ip[i] == '':
+                public.append(f"{i}: --")
+            else:
+                public.append(f"{i}: {public_ip[i]}")
+        public = "\n".join(public)
+        result = "\n\n".join((result, "Public IP addresses", public))
+        return result
+
+    @staticmethod
+    def get_MAC(pkts):
+        """get MAC add table"""
+        mac = {}
+        for p in pkts:
+            if p.haslayer(ARP):
+                mac[p.psrc] = p.hwsrc
+        result = []
+        for i in mac:
+            result.append(f"{i}: {mac[i]}")
+        result = "\n".join(result)
+        result = "\n\n".join(("MAC addresses table:\n\nIP address      MAC address", result))
+        return result
+
+    @staticmethod
+    def save_txt(result, filename):
+        """save txt result"""
+        filename = filename + '.txt'
+        path = os.path.join(os.getcwd(), filename)
+        file = open(path, 'w')
+        file.write(result)
+        file.close()
 	
-# to get pkts related to specific IP
-def find_session(pkts, IP_addr):
-	session = {}
-	for pkt in pkts:
-		if pkt.haslayer(IP):
-			if pkt[IP].src == IP_addr:
-				seg = pkt[IP].payload.name
-				time = str(datetime.datetime.fromtimestamp(float(pkt.time)))
-				session[f"{seg}: {pkt[IP].src} --> {pkt[IP].dst}"] = time
-			elif pkt[IP].dst == IP_addr:
-				seg = pkt[IP].payload.name
-				time = str(datetime.datetime.fromtimestamp(float(pkt.time)))
-				session[f"{seg}: {pkt[IP].dst} <-- {pkt[IP].src}"] = time
-			else:
-				continue
-		else:
-			continue
-
-	result_d = {value : key for (key, value) in session.items()}
-	sort = sorted(result_d)
-	result = []
-	for i in sort:
-		result.append(f"{i}  {result_d[i]}")
-	result = "\n".join(result)
-	result = "\n\n".join((f"Unique connections from {IP_addr} OR to {IP_addr} sorted by time", result))
-	return result
-
+    @staticmethod
+    def save_pcap(pkts, filename):
+        """save pcap result"""
+        filename = filename + '.pcap'
+        path = os.path.join(os.getcwd(), filename)
+        wrpcap(path, pkts)
 	
-
-# to get pkts related to specific session between 2 ip
-def session(pkts, IP_addr_1, IP_addr_2):
-	session = []
-	packets = PacketList()
-	for pkt in pkts:
-		if pkt.haslayer(IP) and not pkt.haslayer(ICMP):
-			if pkt[IP].src == IP_addr_1 and pkt[IP].dst == IP_addr_2:
-				time = datetime.datetime.fromtimestamp(float(pkt.time))
-				seg = pkt[IP].payload.name
-				sport = pkt[seg].sport
-				dport = pkt[seg].dport
-				p_number = pkts.index(pkt)+1
-				session.append(f"#{p_number} {time}  {seg}:  {pkt[IP].src}:{sport} --> {pkt[IP].dst}:{dport}")
-				packets.append(pkt)
-			elif pkt[IP].src == IP_addr_2 and pkt[IP].dst == IP_addr_1:
-				time = datetime.datetime.fromtimestamp(float(pkt.time))
-				seg = pkt[IP].payload.name
-				sport = pkt[seg].sport
-				dport = pkt[seg].dport
-				p_number = pkts.index(pkt)+1
-				session.append(f"#{p_number} {time}  {seg}:  {pkt[IP].dst}:{dport} <-- {pkt[IP].src}:{sport}")
-				packets.append(pkt)
-			else:
-				continue
-		elif pkt.haslayer(IP) and pkt.haslayer(ICMP):
-			if pkt[IP].src == IP_addr_1 and pkt[IP].dst == IP_addr_2:
-				time = datetime.datetime.fromtimestamp(float(pkt.time))
-				seg = pkt[IP].payload.name
-				p_number = pkts.index(pkt)+1
-				session.append(f"#{p_number} {time}  {seg}:  {pkt[IP].src} --> {pkt[IP].dst}")
-				packets.append(pkt)
-			elif pkt[IP].src == IP_addr_2 and pkt[IP].dst == IP_addr_1:
-				time = datetime.datetime.fromtimestamp(float(pkt.time))
-				seg = pkt[IP].payload.name
-				p_number = pkts.index(pkt)+1
-				session.append(f"#{p_number} {time}  {seg}:  {pkt[IP].dst} <-- {pkt[IP].src}")
-				packets.append(pkt)
-			else:
-				continue
-		else:
-			continue
-	session = "\n".join(session)
-	result = "\n\n".join((f"Packets exchange between {IP_addr_1} and {IP_addr_2}", session))
-	return result, packets
-
-#private and public IP
-def list_of_IP(pkts):
-	unique_ip = []
-	private_ip = []
-	public_ip = {}
-	dns_A = PacketList()
-	dns_ans = {}
-	prv_ip = re.compile(r"(10\.\d{1,3}\.\d{1,3}\.\d{1,3})|(172\.((1[6-9])|2([0-9])|(3[0-2]))\.\d{1,3}\.\d{1,3})|(192\.168\.\d{1,3}\.\d{1,3})")
-	for p in pkts[IP]:
-		if p.haslayer(IP):
-			f = p[IP]
-			src = f[IP].src
-			dst = f[IP].dst
-			unique_ip.append(src)
-			unique_ip.append(dst)
-			if f.payload.name == "UDP" and f[UDP].sport == 53 and f[DNS].an != None and f[DNS].an.type == 1:
-				dns_A.append(p)
-	for i in set(unique_ip):
-		mo = prv_ip.search(i)
-		if mo == None:
-			public_ip[i] = ''
-		else:
-			private_ip.append(mo.group())
-	for p in dns_A:
-		dns_ans[p[DNS].an.rdata] = p[DNS].an.rrname.decode("utf-8")
-	for i in dns_ans.keys():
-		if i in public_ip:
-			public_ip[i] = dns_ans[i]
-	private_ip = "\n".join(private_ip)
-	result = "\n\n".join(("Private IP addresses", private_ip))
-	public = []
-	for i in public_ip:
-		if public_ip[i] == '':
-			public.append(f"{i}: --")
-		else:
-			public.append(f"{i}: {public_ip[i]}")
-	public = "\n".join(public)
-	result = "\n\n".join((result, "Public IP addresses", public))
-	return result
-
-#get MAC add table
-def get_MAC(pkts):
-	mac = {}
-	for p in pkts:
-		if p.haslayer(ARP):
-			mac[p.psrc] = p.hwsrc
-	result = []
-	for i in mac:
-		result.append(f"{i}: {mac[i]}")
-	result = "\n".join(result)
-	result = "\n\n".join(("MAC addresses table:\n\nIP address      MAC address", result))
-	return result
-
-#save txt result
-def save_txt(result, filename):
-	filename = filename + '.txt'
-	path = os.path.join(os.getcwd(), filename)
-	file = open(path, 'w')
-	file.write(result)
-	file.close()
-	
-#save pcap result
-def save_pcap(pkts, filename):
-	filename = filename + '.pcap'
-	path = os.path.join(os.getcwd(), filename)
-	wrpcap(path, pkts)
-	
-#check for malicious IP	
-def malicious_ip(pkts):
-	ip_add = []
-	mal_ip = []
-	for i in pkts:
-		if i.haslayer(IP):
-			ip_add.append(i[IP].src)
-			ip_add.append(i[IP].dst)
-		else:
-			continue
-	ip_add = list(set(ip_add))
-	file = open(os.path.join(os.getcwd(), 'ip_mal.txt'), 'r')
-	mal_ip = file.readlines()
-	file.close()
-	result = []
-	for i in ip_add:
-		if i+'\n' in mal_ip:
-			result.append(i)
-	if len(result) == 0:
-		return "No malicious IP addresses found"
-	else:
-		result = "\n".join(result)
-		result = "\n\n".join(("Malicious IP", result))
-		return result
+    @staticmethod
+    def malicious_ip(pkts):
+        """check for malicious IP"""
+        ip_add = []
+        mal_ip = []
+        for i in pkts:
+            if i.haslayer(IP):
+                ip_add.append(i[IP].src)
+                ip_add.append(i[IP].dst)
+            else:
+                continue
+        ip_add = list(set(ip_add))
+        file = open(os.path.join(os.getcwd(), 'ip_mal.txt'), 'r')
+        mal_ip = file.readlines()
+        file.close()
+        result = []
+        for i in ip_add:
+            if i+'\n' in mal_ip:
+                result.append(i)
+        if len(result) == 0:
+            return "No malicious IP addresses found"
+        else:
+            result = "\n".join(result)
+            result = "\n\n".join(("Malicious IP", result))
+            return result
 		
-# list of services
-def get_service(pkts):
-	ports = []
-	port_to_serv = []
-	for p in pkts:
-		if p.haslayer(TCP) or p.haslayer(UDP):
-			src = int(p.sport)
-			if src < 2000:
-				ports.append(src)
-	ports = list(set(ports))
-	for i in ports:
-		try:
-			s = socket.getservbyport(i)
-			port_to_serv.append(f"Port {i}: service {s}")
-		except:
-			continue
-	result = "\n".join(port_to_serv)
-	result = "\n\n".join(("Port numbers to service names:", result))
-	return result
+    @staticmethod
+    def get_service(pkts):
+        """list of services"""
+        ports = []
+        port_to_serv = []
+        for p in pkts:
+            if p.haslayer(TCP) or p.haslayer(UDP):
+                src = int(p.sport)
+                if src < 2000:
+                    ports.append(src)
+        ports = list(set(ports))
+        for i in ports:
+            try:
+                s = socket.getservbyport(i)
+                port_to_serv.append(f"Port {i}: service {s}")
+            except:
+                continue
+        result = "\n".join(port_to_serv)
+        result = "\n\n".join(("Port numbers to service names:", result))
+        return result
 	
 ## actual program ##
 
@@ -207,6 +215,8 @@ args = parser.parse_args()
 path = args.file	
 
 #begin
+
+analyzer = Analyzer()
 
 if not os.path.exists(path):
 	print("Wrong path to the file")
@@ -250,13 +260,13 @@ while (user_choice != '7'):
 		print("Wrong choice")
 		user_choice = input("Select a function: ")
 	if user_choice == '1':
-		result = list_of_IP(pkts)
+		result = analyzer.list_of_IP(pkts)
 		print('\n' + result + '\n')
 		print('Do you want to save the result in txt file? yes/no')
 		s = input('Answer: ')
 		if s == 'yes':
 			filename = input('Enter the name of the file: ')
-			save_txt(result, filename)
+			analyzer.save_txt(result, filename)
 			print('File saved')
 			print('\n' + what_to_do)
 			user_choice = input("Select a function: ")
@@ -264,13 +274,13 @@ while (user_choice != '7'):
 			print('\n' + what_to_do)
 			user_choice = input("Select a function: ")
 	elif user_choice == '2':
-		result = get_MAC(pkts)
+		result = analyzer.get_MAC(pkts)
 		print('\n' + result + '\n')
 		print('Do you want to save the result in txt file? yes/no')
 		s = input('Answer: ')
 		if s == 'yes':
 			filename = input('Enter the name of the file: ')
-			save_txt(result, filename)
+			analyzer.save_txt(result, filename)
 			print('File saved')
 			print('\n' + what_to_do)
 			user_choice = input("Select a function: ")
@@ -284,13 +294,13 @@ while (user_choice != '7'):
 			print('\n' + what_to_do)
 			user_choice = input("Select a function: ")
 		else:
-			result = find_session(pkts, ip)
+			result = analyzer.find_session(pkts, ip)
 			print('\n' + result + '\n')
 			print('Do you want to save the result in txt file? yes/no')
 			s = input('Answer: ')
 			if s == 'yes':
 				filename = input('Enter the name of the file: ')
-				save_txt(result, filename)
+				analyzer.save_txt(result, filename)
 				print('File saved')
 				print('\n' + what_to_do)
 				user_choice = input("Select a function: ")
@@ -305,19 +315,19 @@ while (user_choice != '7'):
 			print('\n' + what_to_do)
 			user_choice = input("Select a function: ")
 		else:
-			(result, packets) = session(pkts, ip1, ip2)
+			(result, packets) = analyzer.session(pkts, ip1, ip2)
 			print('\n' + result + '\n')
 			print('Do you want to save the result in txt file? yes/no')
 			s = input('Answer: ')
 			if s == 'yes':
 				filename = input('Enter the name of the file: ')
-				save_txt(result, filename)
+				analyzer.save_txt(result, filename)
 				print('File saved')
 				print('Do you want to save the packets exchange in pcap file? yes/no')
 				sp = input('Answer: ')
 				if sp == 'yes':
 					filename = input('Enter the name of the file: ')
-					save_pcap(packets, filename)
+					analyzer.save_pcap(packets, filename)
 					print('File saved')
 					print('\n' + what_to_do)
 					user_choice = input("Select a function: ")
@@ -329,7 +339,7 @@ while (user_choice != '7'):
 				sp = input('Answer: ')
 				if sp == 'yes':
 					filename = input('Enter the name of the file: ')
-					save_pcap(packets, filename)
+					analyzer.save_pcap(packets, filename)
 					print('File saved')
 					print('\n' + what_to_do)
 					user_choice = input("Select a function: ")
@@ -337,27 +347,27 @@ while (user_choice != '7'):
 					print('\n' + what_to_do)
 					user_choice = input("Select a function: ")
 	elif user_choice == '5':
-		result = malicious_ip(pkts)
+		result = analyzer.malicious_ip(pkts)
 		print('\n' + result + '\n')
 		print('Do you want to save the result in txt file? yes/no')
 		s = input('Answer: ')
 		if s == 'yes':
 			filename = input('Enter the name of the file: ')
-			save_txt(result, filename)
+			analyzer.save_txt(result, filename)
 			print('File saved')
 			print('\n' + what_to_do)
 			user_choice = input("Select a function: ")
 		else:
 			print('\n' + what_to_do)
 			user_choice = input("Select a function: ")
-	elif user_choice == '6':
-		result = get_service(pkts)
+	else:
+		result = analyzer.get_service(pkts)
 		print('\n' + result + '\n')
 		print('Do you want to save the result in txt file? yes/no')
 		s = input('Answer: ')
 		if s == 'yes':
 			filename = input('Enter the name of the file: ')
-			save_txt(result, filename)
+			analyzer.save_txt(result, filename)
 			print('File saved')
 			print('\n' + what_to_do)
 			user_choice = input("Select a function: ")
